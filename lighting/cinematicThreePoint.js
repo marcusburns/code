67032f5ad66ec5e205f51bf627db21ae544baa8d
marcusburns <marcus24b@gmail.com>
2025-11-04 15:08:12 +0000

    Update cinematicThreePoint.js

--- lighting/cinematicThreePoint.js
+++ lighting/cinematicThreePoint.js
@@ -1,4 +1,4 @@
-import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.160.1/build/three.module.js';
+import * as THREE from 'https://esm.sh/three@0.157.0';
 
 export function applyLighting(scene, renderer) {
 
