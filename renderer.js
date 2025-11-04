a551b388f8797f91be748ea7708df70abc1a1ebc
marcusburns <marcus24b@gmail.com>
2025-11-04 15:32:35 +0000

    Update renderer.js

--- renderer.js
+++ renderer.js
@@ -1,4 +1,4 @@
-
+import { gsap } from 'https://esm.sh/gsap@3.12.2';
 import * as THREE from 'https://esm.sh/three@0.157.0';
 import { GLTFLoader } from 'https://esm.sh/three@0.157.0/examples/jsm/loaders/GLTFLoader.js';
 import { OrbitControls } from 'https://esm.sh/three@0.157.0/examples/jsm/controls/OrbitControls.js';
